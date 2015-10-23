339bc25c7e750523bcb81775f5c264ba7c0fe2e7
jreadey <jreadey@hdfgroup.org>
2015-10-23 11:34:36 -0700

    run ipcontroller seperately

--- util/ipcontroller.sh
+++ util/ipcontroller.sh
@@ -19,20 +19,12 @@ ENGINE_JSON=ipcontroller-engine.json
 
 echo "checking:" $PROFILE_DIR/$ENGINE_JSON
 
-if [ -e $PROFILE_DIR/$ENGINE_JSON ]
-then
-   echo "engine files exists (controller already running?)"
-   exit 1
-fi
-ipcontroller --profile $profile --ip=* &
-
-# wait for json file to be created
-sleep 3
 if [ ! -e $PROFILE_DIR/$ENGINE_JSON ]
 then
-   echo "engine file not found"
+   echo "engine file not found  (did you run 'ipcontroller --ip=*'?)"
    exit 1
 fi
+ 
 echo "copy engine json to s3"
 s3cmd put --force $PROFILE_DIR/$ENGINE_JSON s3://hdfgroup/.ipython/profile_$profile/security/$ENGINE_JSON
 echo "done!"
