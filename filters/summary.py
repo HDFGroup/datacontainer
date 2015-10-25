8766516d70fc99111605793d5d034ffd1e226efd
jreadey <jreadey@hdfgroup.org>
2015-10-24 21:02:53 -0700

    do s3 downloads in parallel

--- filters/summary.py
+++ filters/summary.py
@@ -2,27 +2,31 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import sys
 import os
+import subprocess
+import time
 import h5py
 import numpy as np
 import argparse
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                             os.path.pardir, 'lib')))
-from s3cache import getS3Object
-
+BATCH_SIZE = 5  # download files this many at a time
+s3_prefix = "s3://"
 
 def summary(file_path, h5path):
-    file_path = file_helper(file_path)
+     
     file_name = os.path.basename(file_path)
+    
+    print("Summary ", file_path, h5path)
 
+    if not h5py.is_hdf5(file_path):
+        raise IOError("Not an HDF5 file: " + file_path)
     with h5py.File(file_path, 'r') as f:
         dset = f[h5path]
 
         # mask fill value
         if '_FillValue' in dset.attrs:
-            tair_2m = dset[...]
+            arr = dset[...]
             fill = dset.attrs['_FillValue'][0]
-            v = tair_2m[tair_2m != fill]
+            v = arr[arr != fill]
         else:
             v = dset[...]
         # file name GSSTF_NCEP.3.YYYY.MM.DD.he5
@@ -30,23 +34,55 @@ def summary(file_path, h5path):
         print(file_name, len(v), np.min(v), np.max(v), np.mean(v),
               np.median(v), np.std(v))
 
-
-def file_helper(filepath):
-    if filepath.startswith("s3://"):
-        filepath = getS3Object(filepath)
-    if not os.path.exists(filepath):
-        raise IOError("filepath " + filepath + " does not exist")
-    if not h5py.is_hdf5(filepath):
-        raise IOError("filepath " + filepath + " is not an HDF5 file")
-
-    return filepath
-
+    
+def dobatch(filelist, **kwargs):
+    downloads = []  # handles to sub-processes
+    s3_cache_dir = os.environ["S3_CACHE_DIR"]
+    downloaded_files = []
+    for filename in filelist:
+        if filename.startswith(s3_prefix):
+            if s3_cache_dir is None:
+                raise IOError("Environment variable S3_CACHE_DIR not set")
+            s3_path = filename[len(s3_prefix):]
+            s3_uri = filename
+            local_filepath = os.path.join(s3_cache_dir, s3_path)
+     
+            if os.path.exists(local_filepath):
+                # todo, check that the s3 object is the same as local copy
+                pass
+            else:
+                p = subprocess.Popen(['s3cmd', 'get', s3_uri, local_filepath])
+                downloads.append(p)
+            downloaded_files.append(local_filepath)
+        else:
+            downloaded_files.append(filename)
+            
+    if len(downloads) > 0:
+        done = False
+        while not done:
+            print('.')
+            time.sleep(1)
+            done = True
+            for p in downloads:
+                p.poll()
+                if p.returncode is None:
+                    done = False # still waiting on a download
+                elif p.returncode < 0:
+                    raise IOError("s3cmd failed for " + filename)
+                else:
+                    pass  # success!
+    print("downloads complete")
+    for filename in downloaded_files:
+        summary(filename, kwargs['h5path'] )        
+     
+    
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', "--file", help="name of file or s3 uri")
-    parser.add_argument('-fl', "--filelist", help="text file of files or s3 uri")
+    parser.add_argument('-f', "--filename", help="name of file or s3 uri")
+    parser.add_argument('-i', "--input", help="text file of files or s3 uri")
     parser.add_argument('-p', "--path", help="h5path")
+    parser.add_argument('-c', "--cluster", help="cluster profile")
     # example file:
     # public AWS -
     # s3://hdfgroup/data/hdf5test/GSSTF_NCEP.3.2000.05.01.he5
@@ -55,24 +91,36 @@ def main():
 
     # example path (for above file):
     # /HDFEOS/GRIDS/NCEP/Data\ Fields/Psea_level
+    
 
     args = parser.parse_args()
 
-    if not args.file and not args.filelist:
+    if not args.filename and not args.input:
         sys.exit("No filename specified!")
 
     if not args.path:
         sys.exit("No h5path specified!")
 
-    if args.filelist:
-        with open(args.filelist) as f:
+    files = []
+    if args.input:
+        with open(args.input) as f:
             for line in f:
                 line = line.strip()
                 if not line or line[0] == '#':
                     continue
-                summary(line, args.path)
+                files.append(line)              
     else:
-        summary(args.file, args.path)
+        files.append(args.filename)
+         
+    
+    batch = []    
+    for filename in files:
+        batch.append(filename)
+        if len(batch) == BATCH_SIZE:
+            dobatch(batch)
+            batch = []
+            
+    dobatch(batch, h5path=args.path)  # catch any trailers
 
 
 main()
--- jobs/ncep_summary.sh
+++ jobs/ncep_summary.sh
@@ -2,4 +2,4 @@
 # construct list of s3 uri's to run
 s3cmd ls s3://hdfdata/ncep3/ | grep -v xml | grep he5 | rev | cut -d: -f1 | rev | sed -e 's/^/s3:/' > ncep_files.txt
 cd ../filters/
-python summary.py --filelist ../jobs/ncep_files.txt --path /HDFEOS/GRIDS/NCEP/Data\ Fields/Tair_2m
+python summary.py --input ../jobs/ncep_files.txt --path /HDFEOS/GRIDS/NCEP/Data\ Fields/Tair_2m
