d01f43a1d45eef422804fa799793d4eeb4a25235
jreadey <jreadey@hdfgroup.org>
2015-11-11 12:26:15 -0800

    remove test code from s3downlaoder file

--- lib/s3downloader.py
+++ lib/s3downloader.py
@@ -244,7 +244,7 @@ class S3Download:
         if download_size > freespace:
             msg = "not enough free space to download: " + str(download_size)
             msg += ", " + str(freespace) + " available"
-            log.info(msg)
+            self.log.info(msg)
             raise IOError(msg)  
         
         count = self.update()
@@ -261,41 +261,7 @@ class S3Download:
             output.append((s3_uri, download['state'] ))
         return output
          
-    
-#main
-
-ncep3_files = ["s3://hdfdata/ncep3/GSSTF_NCEP.3.1987.07.01.he5",
-"s3://hdfdata/ncep3/GSSTF_NCEP.3.1987.07.02.he5",
-"s3://hdfdata/ncep3/GSSTF_NCEP.3.1987.07.03.he5",
-"s3://hdfdata/ncep3/GSSTF_NCEP.3.1987.07.04.he5"]
-
-s3download = S3Download()
-nspace = s3download.freespace()
-print("free space:", nspace)
-usedspace = s3download.usedspace()
-print("used space", usedspace)
-"""
-print("clearing..")
-s3download.clear()
-usedspace = s3download.usedspace()
-print("used space", usedspace)
-
-#s3download.addFiles(ncep3_files)
-s3download.addFiles("s3://hdfdata/ncep3/")
-output = s3download.dump()
-
-#output = s3download.s3cmdls("s3://hdfdata/ncep3/")
-print(output)
-"""
-s3download.addFiles("s3://hdfdata/ncep3/")
-count = s3download.start()
-while count > 0:
-    count = s3download.update()
-    print(s3download.dump())
-    time.sleep(1)
-print("done!")
-
-
+     
 
     
      
