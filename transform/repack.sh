ecfbb23dbd457b2d23886e24d7abceee79f0aa90
H. Joe Lee <hyoklee@hdfgroup.org>
2015-11-18 11:26:50 -0600

    add szip compression for #10.

--- transform/repack.sh
+++ transform/repack.sh
@@ -12,12 +12,19 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     name=${line##*/}
     echo $name
     /home/ubuntu/bin/s3cmd get $line
+    
     # h5py suggested size:
     # h5repack -L -l CHUNK=45x180 -f GZIP=9 $name repacked/$name
+    # /home/ubuntu/bin/s3cmd put repacked/$name s3://hdfdata/ncep3_chunk_45_180_gzip_9/$name
 
     # Unidata suggested size:
-    h5repack -L -l CHUNK=22x46 -f GZIP=9 $name repacked/$name
-    /home/ubuntu/bin/s3cmd put repacked/$name s3://hdfdata/ncep3_chunk_22_46_gzip_9/$name
+    # h5repack -L -l CHUNK=22x46 -f GZIP=9 $name repacked/$name
+    # /home/ubuntu/bin/s3cmd put repacked/$name s3://hdfdata/ncep3_chunk_22_46_gzip_9/$name
+
+    # SZIP. It works only on joe_issue10 snapshot.
+    /home/ubuntu/anaconda/bin/h5repack -L -l CHUNK=45x180 -f SZIP=8,NN $name repacked/$name
+    /home/ubuntu/bin/s3cmd put repacked/$name s3://hdfdata/ncep3_chunk_45_180_szip_8_NN/$name
+    
     rm -f repacked/$name
     rm -f $name
 done < "$1"
