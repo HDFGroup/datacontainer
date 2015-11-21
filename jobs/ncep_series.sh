826e106524535399ba2b11490b75ab2d8f72006d
jreadey <jreadey@hdfgroup.org>
2015-11-20 21:54:30 -0800

    added job for time series

---
+++ jobs/ncep_series.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+cd ../util
+python s3pd.py get s3://hdfdata/ncep3/\*.he5   
+cd ../filters/
+python series.py --file s3://hdfdata/ncep3 --path /HDFEOS/GRIDS/NCEP/Data\ Fields/Tair_2m --lat 444 --long 555
