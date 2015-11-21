30704949f6695de3de23f88a410bf14ea6db24e3
jreadey <jreadey@hdfgroup.org>
2015-11-20 21:45:51 -0800

    remove parens from summary output

--- jobs/ncep_summary.sh
+++ jobs/ncep_summary.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 cd ../util
-python s3pd.py get s3://hdfdata/ncep3/   
+python s3pd.py get s3://hdfdata/ncep3/\*.he5   
 cd ../filters/
 python summary.py --file s3://hdfdata/ncep3 --path /HDFEOS/GRIDS/NCEP/Data\ Fields/Tair_2m
