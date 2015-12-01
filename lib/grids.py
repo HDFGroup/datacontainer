9babd95f8572b5392a43646ae23450a1de53cc4b
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2015-12-01 00:30:39 -0500

    Many improvements to grids.py's classes

--- lib/grids.py
+++ lib/grids.py
@@ -4,7 +4,22 @@ from collections import namedtuple
 
 
 class DataPoint:
-    """Base class describing a spatio-temporal properties of a data point."""
+    """Base class describing a spatio-temporal properties of a data point.
+
+    DataPoint objects can be created in two ways::
+
+        >>> d = DataPoint(-77.45, 35.67, '2001-07-14T12:34:56')
+
+    where all the arguments are positional in the order: latitude, longitude,
+    time, or::
+
+        >>> d = DataPoint(lat=-77.45, lon=35.67, time=datetime(2001, 7, 14, 12, 34, 56))
+
+    with all named arguments. Mixing these two styles is not allowed.
+
+    The time can be either an ISO 8601 string with no fractional seconds and
+    time zone or a naive ``datetime()`` object.
+    """
 
     def __init__(self, *args, **kwargs):
         if args and kwargs:
@@ -12,11 +27,12 @@ class DataPoint:
                 'Only all positional or all named arguments allowed')
         elif args:
             if len(args) != 3:
-                raise RuntimeError('Two arguments expected')
-            self._lat, self._lon, self.time = args
+                raise RuntimeError('Three arguments expected: lat, lon, time')
+            self._lat, self._lon, self._time = args
         elif kwargs:
             if any([k not in kwargs for k in ('lat', 'lon', 'time')]):
-                raise RuntimeError('Two arguments "lat" and "lon" expected')
+                raise RuntimeError(
+                    'Three arguments "lat", "lon", and "time" expected')
             self._lat = kwargs['lat']
             self._lon = kwargs['lon']
             self._time = kwargs['time']
@@ -59,6 +75,13 @@ class DataPoint:
     def time_idx(self):
         return int((self._time - self.time_start) / self.time_res)
 
+    @property
+    def filename(self):
+        """Return the name of the file where the data point is stored."""
+        dt = self.time
+        return self._fname_template.format(year=dt.year, month=dt.month,
+                                           day=dt.day)
+
 
 class GsstfNcep:
     """Describe the properties of the NCEP/DOE Reanalysis II,
@@ -66,6 +89,8 @@ class GsstfNcep:
 
     https://catalog.data.gov/dataset/ncep-doe-reanalysis-ii-for-gsstf-0-25x0-25-deg-daily-grid-v3-gsstf-ncep-at-ges-disc-v3
     """
+    # Data set's file name template...
+    _fname_template = 'GSSTF_NCEP.3.{year:04d}.{month:02d}.{day:02d}.he5'
 
     @property
     def lon_res(self):
@@ -121,7 +146,7 @@ class GsstfNcep:
         return 1440
 
 
-class NcepDataPoint(GsstfNcep, DataPoint):
+class GsstfDataPoint(GsstfNcep, DataPoint):
     """Define a data point of the GSSTF NCEP data set."""
     pass
 
@@ -146,27 +171,72 @@ class DataRegion:
         lons = [d.lon for d in dpoints]
         lats = [d.lat for d in dpoints]
         times = [d.time for d in dpoints]
-        GeoBBox = namedtuple(
-            'GeoBBox',
-            ['west', 'east', 'north, ''south', 'begin', 'end'])
-        self._geobbox = GeoBBox(min(lons), max(lons), max(lats), min(lats),
-                                min(times), max(times))
+        DataBBox = namedtuple(
+            'DataBBox',
+            ['min_lon', 'max_lon', 'max_lat', 'min_lat', 'begin', 'end'])
+        self._databbox = DataBBox(min(lons), max(lons), max(lats), min(lats),
+                                  min(times), max(times))
         lons_idx = [d.lon_idx for d in dpoints]
         lats_idx = [d.lat_idx for d in dpoints]
         times_idx = [d.time_idx for d in dpoints]
         IndexBBox = namedtuple(
             'IndexBBox',
-            ['west', 'east', 'north, ''south', 'begin', 'end'])
+            ['min_lon', 'max_lon', 'max_lat', 'min_lat', 'begin', 'end'])
         self._idxbbox = IndexBBox(min(lons_idx), max(lons_idx),
                                   max(lats_idx), min(lats_idx),
                                   min(times_idx), max(times_idx))
 
+        self._dpoints = dpoints
+
     @property
-    def geobbox(self):
-        """Geo bounding box as a namedtuple."""
-        return self._geobbox
+    def databbox(self):
+        """Data bounding box as a namedtuple. The namedtuple's fields are:
+        ``min_lon``, ``max_lon``, ``min_lat``, ``max_lat``, ``begin``, and
+        ``end``.
+        """
+        return self._databbox
 
     @property
     def idxbbox(self):
-        """Dataspace (index) bounding box as a namedtuple."""
+        """Dataspace (index) bounding box as a namedtuple. The namedtuple's
+        fields are: ``min_lon``, ``max_lon``, ``min_lat``, ``max_lat``,
+        ``begin``, and ``end``.
+        """
         return self._idxbbox
+
+    def files(self):
+        """A list of file names containing the data from the bounding box.
+
+        :return: A generator that produces file names for the extent of the
+            bounding box.
+
+        Note the number of generated file names will equal the number of data
+        points within the bounding box. There could be a lot of data points and
+        many of their file names might be duplicates. Using Python's sets is
+        recommended to only keep the unique file names, for example::
+
+            >>> r = DataRegion([...])
+            >>> unique_sorted_files = sorted(set(r.files()))
+        """
+        # The class of actual DataPoint objects...
+        cls = type(self._dpoints[0])
+
+        # Regenerate all the DataPoints within the data bounding box and return
+        # their file names...
+        idx = self.idxbbox
+        time = self.databbox.begin
+        for t in range(idx.begin, idx.end+1):
+            lat = self._databbox.min_lat
+            for lt in range(idx.min_lat, idx.max_lat+1):
+                lon = self._databbox.min_lon
+                for ln in range(idx.min_lon, idx.max_lon+1):
+                    dp = cls(lat=lat, lon=lon, time=time)
+                    yield dp.filename
+                    lon += self.lon_res
+                lat += self.lat_res
+            time += self.time_res
+
+
+class GsstfDataRegion(GsstfNcep, DataRegion):
+    """Define a data region of the GSSTF NCEP data set."""
+    pass
