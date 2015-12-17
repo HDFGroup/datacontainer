83a4f3ff09fc94377ef28aa60db2a553f45c120e
jreadey <jreadey@hdfgroup.org>
2015-12-16 21:23:51 -0800

    fixed typo

--- jobs/ncep_series.sh
+++ jobs/ncep_series.sh
@@ -3,4 +3,4 @@
 cd ../util
 python s3pd.py get s3://hdfdata/ncep3/\*.he5   
 cd ../filters/
-python series.py --file s3://hdfdata/ncep3 --path /HDFEOS/GRIDS/NCEP/Data\ Fields/Tair_2m --lat 444 --long 555
+python series.py --file s3://hdfdata/ncep3 --path /HDFEOS/GRIDS/NCEP/Data\ Fields/Tair_2m --lat 444 --lon 555
