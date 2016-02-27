5decb3dcc477ed8d4c24196a15f5777ff59005d8
jreadey <jreadey@hdfgroup.org>
2016-02-26 16:09:38 -0800

    fix script to run in powers of 2

--- jobs/ncep_summary_h5serv_par.sh
+++ jobs/ncep_summary_h5serv_par.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #
-# run ipcluster before launching this script
-MAXNODES=4
+# run ipcluster with 32 engines before launching this script
 ENDPOINT="http://127.0.0.1:5000"
 FILE="ncep3_25_20_20_gzip9"
 DOMAIN=$FILE".hdfgroup.org"
 cd ../filters/
-for (( i=1; i <= $MAXNODES; i++ ))
+for i in 1 2 4 8 16 32
 do
     #echo "nodes:" $i
     #echo "domain: ["$DOMAIN"]"
